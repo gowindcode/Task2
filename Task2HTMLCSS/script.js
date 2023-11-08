48346a3320f020c271ed6540c6e181a3d8bfa6a6
gowindcode <gowindcan@gmail.com>
2023-11-09 02:11:49 +0530

    images url upated

---
+++ Task2HTMLCSS/script.js
@@ -0,0 +1,10 @@
+// script.js
+
+// document.addEventListener("DOMContentLoaded", function() {
+//     const showImageBtn = document.getElementById("showImageBtn");
+//     const imageElement = document.getElementById("imageElement");
+
+//     showImageBtn.addEventListener("click", function() {
+//         imageElement.style.display = "block";
+//     });
+// });
